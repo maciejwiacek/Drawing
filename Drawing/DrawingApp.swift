63e1f996b053bab6b50bbbb0f5196a24dde3b335
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-09-18 15:40:10 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Maciej Wiącek on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
